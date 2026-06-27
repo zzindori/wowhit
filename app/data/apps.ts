d4054b417e04b5e5cb1b811a2dfc5dbc65637d93
zzindori <zzindori5@gmail.com>
2026-06-27 10:09:30 +0900

    feat: itNe v1.5.5+32 버전 업

--- app/data/apps.ts
+++ app/data/apps.ts
@@ -84,9 +84,9 @@ export const apps: App[] = [
     api: ['Gemini', 'MLKit'],
     iconUrl: '/apps/itne/icon.png',
     webUrl: 'https://itne.wowhit.org',
-    apkUrl: 'https://github.com/zzindori/wowhit-releases/releases/download/itne-v1.5.4.31/app-release.apk',
+    apkUrl: 'https://github.com/zzindori/wowhit-releases/releases/download/itne-v1.5.5.32/app-release.apk',
     purchaseUrl: 'https://smartstore.naver.com/wowhit/products/13644369839',
-    version: '1.5.4+31'
+    version: '1.5.5+32'
   },
   {
     slug: 'careway',
